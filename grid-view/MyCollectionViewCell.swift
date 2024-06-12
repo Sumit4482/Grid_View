277a904575c5e422883e9a4b1dca9c4c3e48b980
Sumit Banwkade <sbanwakde4482@gmail.com>
2024-06-12 23:29:19 +0530

    Initial Commit

---
+++ grid-view/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  grid-view
+//
+//  Created by E5000855 on 04/06/24.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
